5b3da819ed03cf0487f419dd6d141685cf660314
Sabrina Nardin <nardin@uchicago.com>
2022-10-19 10:30:32 -0500

    Updated instructions

--- 01-relational-data-solution.R
+++ 01-relational-data-solution.R
@@ -11,11 +11,12 @@ library(tidyverse)
 
 # QUESTION 1. Is there a relationship between the age of a plane and its departure delays?
 
-# step 0: inspect the data frames -- which variables do you need to solve the problem?
+# step 1: inspect the data frames -- which variables do you need to solve the problem?
+# this is a key step, spend some time looking at these two tables and the variables they have in common
 glimpse(flights)
 glimpse(planes)
 
-# step 1: 
+# step 2: 
 # use mutate() and the appropriate variables to calculate age using the planes table 
 # then select the required variables from the planes table
 plane_ages <- planes %>%
@@ -23,7 +24,7 @@ plane_ages <- planes %>%
     select(tailnum, age)
 plane_ages
 
-# step 2: 
+# step 3: 
 # use inner_join() to combine the data frames (flights and plane_ages)
 # generate a graph to shows the relationship between the required variables, use a smoothing line geom
 flights %>%
@@ -35,19 +36,20 @@ flights %>%
 
 # QUESTION 2. Add the location of the origin and destination (i.e. the `lat` and `lon`) to `flights`
 
-# step 0: inspect the data frames -- which variables do you need to solve the problem?
+# step 1: inspect the data frames -- which variables do you need to solve the problem?
+# this is a key step, spend some time looking at these two tables and the variables they have in common 
 glimpse(flights)
 glimpse(airports)
 
-# step 1: select only the necessary columns from airports, save them in airports_lite
+# step 2: select only the necessary columns from airports, save them in airports_lite
 airports_lite <- airports %>%
   select(faa, lat, lon)
 airports_lite
 
-# step 2: combine the columns using left_join()
+# step 3: combine the columns using left_join()
 # specify the names of the matching columns, and distinguish destination from origin
 # save your results in a new data frame
-# hint: to distinguish distention from origin you need to add the suffix argument
+# hint: to distinguish destination from origin you need to add the suffix argument
 # see https://dplyr.tidyverse.org/reference/mutate-joins.html
 joined <- flights %>%
   left_join(y = airports_lite, by = c(dest = "faa")) %>%
--- 01-relational-data.R
+++ 01-relational-data.R
@@ -11,18 +11,19 @@ library(tidyverse)
 
 # QUESTION 1. Is there a relationship between the age of a plane and its departure delays?
 
-# step 0: inspect the data frames -- which variables do you need to solve the problem?
+# step 1: inspect the data frames -- which variables do you need to solve the problem?
+# this is a key step, spend some time looking at these two tables and the variables they have in common 
 glimpse(flights)
 glimpse(planes)
 
-# step 1: 
+# step 2: 
 # use mutate() and the appropriate variables to calculate age using the planes table 
 # then select the required variables from the planes table
 plane_ages <- ________ %>%
     mutate( ________ = 2013 - ________ ) %>%
     select( _______ , _________ )
 
-# step 2: 
+# step 3: 
 # use inner_join() to combine the data frames (flights and plane_ages)
 # generate a graph to shows the relationship between the required variables, use a smoothing line geom
 _______ %>%
@@ -34,17 +35,18 @@ _______ %>%
 
 # QUESTION 2. Add the location of the origin and destination (i.e. `lat` and `lon`) to flights
 
-# step 0: inspect the data frames -- which variables do you need to solve the problem?
+# step 1: inspect the data frames -- which variables do you need to solve the problem?
+# this is a key step, spend some time looking at these two tables and the variables they have in common 
 glimpse(flights)
 glimpse(airports)
 
-# step 1: select only the necessary columns from airports, save them in airports_lite
+# step 2: select only the necessary columns from airports, save them in airports_lite
 
 
-# step 2: combine the columns using left_join()
+# step 3: combine the columns using left_join()
 # specify the names of the matching columns, and distinguish destination from origin
-# save your results in a new dataframe
-# hint: to distinguish distention from origin you need to add the suffix argument
+# save your results in a new data frame
+# hint: to distinguish destination from origin you need to add the suffix argument
 # see https://dplyr.tidyverse.org/reference/mutate-joins.html
 
 
